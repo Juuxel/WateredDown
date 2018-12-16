fd73425ec10feff7c102421a4f342a663d2cb989
Juuxel <kasperi.kauppi@gmail.com>
2018-12-16 20:59:04 +0200

    Revert the change to sand waterlogging

--- src/main/java/juuxel/watereddown/mixin/SandMixin.java
+++ src/main/java/juuxel/watereddown/mixin/SandMixin.java
@@ -4,15 +4,12 @@
  */
 package juuxel.watereddown.mixin;
 
-import juuxel.watereddown.api.FluidProperty;
-import juuxel.watereddown.api.Fluidloggable;
-import juuxel.watereddown.api.WDProperties;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SandBlock;
+import net.minecraft.block.*;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
+import net.minecraft.state.property.Properties;
+import net.minecraft.tag.FluidTags;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,21 +19,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SandBlock.class)
-@Implements(@Interface(iface = Fluidloggable.class, prefix = "waterlog$"))
+@Implements(@Interface(iface = Waterloggable.class, prefix = "waterlog$"))
 public abstract class SandMixin extends BlockMixin {
     @Inject(at = @At("RETURN"), method = "<init>")
     private void onConstruct(int var1, Block.Settings var2, CallbackInfo info) {
-        setDefaultState(getDefaultState().with(WDProperties.FLUID, FluidProperty.EMPTY));
+        setDefaultState(getDefaultState().with(Properties.WATERLOGGED, false));
     }
 
     @Override
     protected void appendProperties(StateFactory.Builder<Block, BlockState> var1, CallbackInfo info) {
-        var1.with(WDProperties.FLUID);
+        var1.with(Properties.WATERLOGGED);
     }
 
     @Override
     protected void getPlacementState(ItemPlacementContext context, CallbackInfoReturnable<BlockState> info) {
-        FluidState state = context.getWorld().getFluidState(context.getPos());
-        info.setReturnValue(info.getReturnValue().with(WDProperties.FLUID, new FluidProperty.Wrapper(state.getFluid())));
+        try {
+            FluidState state = context.getWorld().getFluidState(context.getPos());
+            info.setReturnValue(info.getReturnValue().with(Properties.WATERLOGGED, state.matches(FluidTags.WATER)));
+        } catch (NullPointerException e) {}
     }
 }
