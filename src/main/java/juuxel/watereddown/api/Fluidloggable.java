11b4c3606c423d896636c75eaadbd086e24d3a6f
Juuxel <kasperi.kauppi@gmail.com>
2018-12-16 21:09:27 +0200

    Fix the header of a class + release 0.2.0

--- src/main/java/juuxel/watereddown/api/Fluidloggable.java
+++ src/main/java/juuxel/watereddown/api/Fluidloggable.java
@@ -1,6 +1,6 @@
-/* This file is a part of the This & That project
+/* This file is a part of the Watered Down project
  * by Juuxel, licensed under the MIT license.
- * Full code and license: https://github.com/Juuxel/ThisAndThat
+ * Full code and license: https://github.com/Juuxel/WateredDown
  */
 package juuxel.watereddown.api;
 
